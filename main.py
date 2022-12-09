6a32d96d4b2569b119a49f81dc6586c2efbf7328
Samuel Haile <samhaile129@gmail.com>
2022-12-09 06:58:36 -0500

    Created tkinter label

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ window.title('My First GUI Program')
 # Minimum size of window
 window.minsize(width= 500, height= 300)
 
+# This creates a Label
+my_label = tkinter.Label(text="This is a Label", font=("Arial", 24, "bold"))
+#this displays and centers it
+my_label.pack(expand=True)
 
 
 
