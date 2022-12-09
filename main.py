c0523cdc05801c78160d9ac413249a61654231d3
Samuel Haile <samhaile129@gmail.com>
2022-12-08 22:20:04 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter
+
+window = tkinter.Tk()
+
+#Title of window
+window.title('My First GUI Program')
+
+# Minimum size of window
+window.minsize(width= 500, height= 300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Method that keeps window open
+window.mainloop()
